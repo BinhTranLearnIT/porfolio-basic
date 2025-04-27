14aeeae16dcc786fc3774bb0d1b19bd5c476a4f0
Binh Tran <tranngocbaobinh01@gmail.com>
2025-04-27 08:51:35 +0700

    continue

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//  <-- Header Navbar -->
+const links = document.querySelectorAll("#navbar a");
+
+links.forEach((link) => {
+  link.addEventListener("click", function () {
+    // Xoá active ở tất cả link
+    console.log();
+    event.preventDefault();
+    links.forEach((l) => l.classList.remove("a--active"));
+    // Gán active cho link vừa click
+    this.classList.add("a--active");
+  });
+});
+
+//  <--  Navbar Scroll -->
+window.addEventListener("scroll", function () {
+  const header = document.getElementById("header");
+  if (window.scrollY > 60) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
